a7f7ed8f012059f8cc283b704f28883e4a917cb2
asleboon <asle.berge@gmail.com>
2025-12-14 13:57:48 +0100

    fixed it!

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,32 @@
--- lua/plugins/treesitter.lua
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require('nvim-treesitter').setup {
       ensure_installed = {
-        -- Editor / config
         'lua',
-        'luadoc',
         'vim',
         'vimdoc',
         'query',
-
-        -- C#
         'c_sharp',
-
-        -- JS / TS / React / Next
         'javascript',
         'typescript',
         'tsx',
-
-        -- Web
         'html',
         'css',
-
-        -- Data / config
         'json',
         'yaml',
-
-        -- Docs
         'markdown',
         'markdown_inline',
+        'lua',
+        'vim',
+        'javascript',
+        'html',
+        'css',
+        'python',
       },
-
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-      },
-
-      indent = {
-        enable = true,
-        -- Known-problematic
-        disable = { 'yaml', 'markdown' },
-      },
+      highlight = { enable = true },
+      indent = { enable = true },
     }
   end,
 }
