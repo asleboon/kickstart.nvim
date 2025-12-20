92e88784596b45befaa7e65d209186fe2b22f1e5
asleboon <asle.berge@gmail.com>
2025-12-20 10:45:22 +0100

    Update autocomplete config

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,50 +1,18 @@
 return {
-  'saghen/blink.cmp',
-  event = 'VimEnter',
   version = '1.*',
+  'saghen/blink.cmp',
   dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      opts = {},
-    },
     'folke/lazydev.nvim',
+    'rafamadriz/friendly-snippets',
+    'L3MON4D3/LuaSnip',
   },
   opts = {
-    keymap = {
-      preset = 'default',
-      ['<C-l>'] = { 'show', 'hide' },
-      ['<CR>'] = { 'accept', 'fallback' }, -- ENTER confirms completion
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-
-      ['<C-n>'] = { 'select_next' },
-      ['<C-p>'] = { 'select_prev' },
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+    appearance = {
+      nerd_font_variant = 'mono',
     },
-    appearance = { nerd_font_variant = 'mono' },
-    completion = {
-      documentation = {
-        auto_show = false,
-        auto_show_delay_ms = 500,
-      },
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lazydev = {
-          module = 'lazydev.integrations.blink',
-          score_offset = 100,
-        },
-      },
-    },
-    snippets = { preset = 'luasnip' },
-    fuzzy = { implementation = 'lua' },
     signature = { enabled = true },
+    completion = { documentation = { auto_show = true } },
   },
 }
